e505a9e3648489d78350bf8140bd76b19e03b518
Derek Passen <dpassen1@gmail.com>
2017-10-21 12:59:04 -0500

    No need to construct intermediate list here.

--- tests/matchers.py
+++ tests/matchers.py
@@ -1,5 +1,5 @@
 def HasFields(cls, fields):
     class HasFields(cls):
         def __eq__(self, other):
-            return all([other[key] == fields[key] for key in fields.keys()])
+            return all(other[key] == fields[key] for key in fields.keys())
     return HasFields()
